7c66ffaf79ed94936d0d6f1803947fbd7d488676
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-11-22 22:19:10 -0600

    first deploy

---
+++ login.php
@@ -0,0 +1,15 @@
+<!doctype html>
+<html> 
+    <head>
+        <title></title>
+    </head>
+    <body>
+        <form action="do_login.php" method="POST">
+            <p>Username:</p>
+            <input name="username" type="text">
+            <p>Password:</p>
+            <input name="password" type="password">
+            <input type="submit" value="Submit">
+        </form> 
+    </body>
+</html>
