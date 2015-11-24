51260631d70b9c6ab9cc97d79b5d1547356beaf8
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-11-24 12:03:00 -0600

    ja;dlkf

--- do_login.php
+++ do_login.php
@@ -4,12 +4,12 @@
     print $_POST['username'];
 
      //HTTPS redirect
-    if ($_SERVER['HTTPS'] !== 'on') {
-		$redirectURL = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-		header("Location: $redirectURL");
-		exit;
-	}
-	
+//    if ($_SERVER['HTTPS'] !== 'on') {
+//		$redirectURL = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+//		header("Location: $redirectURL");
+//		exit;
+//	}
+//	
 	if(!session_start()) {
 		// If the session couldn't start, present an error
 		header("Location: error.php");
