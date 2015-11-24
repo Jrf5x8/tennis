c1b1de93829ed7f9ed376f8d4f65490f26683375
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-11-24 12:01:51 -0600

    ads;f

--- do_login.php
+++ do_login.php
@@ -3,32 +3,34 @@
     print_r($_POST);
     print $_POST['username'];
 
-    // HTTPS redirect
-//    if ($_SERVER['HTTPS'] !== 'on') {
-//		$redirectURL = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
-//		header("Location: $redirectURL");
-//		exit;
-//	}
-//	
-//	if(!session_start()) {
-//		// If the session couldn't start, present an error
-//		header("Location: error.php");
-//		exit;
-//	}
-//
-//
-//
-//	// Check to see if the user has already logged in
-//	$loggedIn = empty($_SESSION['loggedin']) ? false : $_SESSION['loggedin'];
-//	
-//	if ($loggedIn) {
-//		header("Location: page1.php");
-//		exit;
-//	}
-//
-//
-//    $username = $_POST['username'];
-//    $password = $_POST['password'];
+     //HTTPS redirect
+    if ($_SERVER['HTTPS'] !== 'on') {
+		$redirectURL = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+		header("Location: $redirectURL");
+		exit;
+	}
+	
+	if(!session_start()) {
+		// If the session couldn't start, present an error
+		header("Location: error.php");
+		exit;
+	}
+
+
+
+	// Check to see if the user has already logged in
+	$loggedIn = empty($_SESSION['loggedin']) ? false : $_SESSION['loggedin'];
+	
+	if ($loggedIn) {
+		header("Location: page1.php");
+		exit;
+	}
+
+
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+
+    print "The username is: $username";
 //
 //    require_once db.conf;
 //
