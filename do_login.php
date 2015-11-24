e579eab0216e6ef2eafe7bcbc707b6dc3241cc18
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-11-23 22:41:14 -0600

    jack

--- do_login.php
+++ do_login.php
@@ -9,18 +9,22 @@
 		exit;
 	}
 	
-//	if(!session_start()) {
-//		// If the session couldn't start, present an error
-//		header("Location: error.php");
-//		exit;
-//	}
+	if(!session_start()) {
+		// If the session couldn't start, present an error
+		header("Location: error.php");
+		exit;
+	}
 
-    session_start();
 
-    if($_SESSION['username']){
-        header("Location: home.php");
-        exit;
-    }
+
+	// Check to see if the user has already logged in
+	$loggedIn = empty($_SESSION['loggedin']) ? false : $_SESSION['loggedin'];
+	
+	if ($loggedIn) {
+		header("Location: page1.php");
+		exit;
+	}
+
 
     $username = $_POST['username'];
     $password = $_POST['password'];
