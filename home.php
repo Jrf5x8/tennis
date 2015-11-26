fac5e9e5134744285163acd69e13134b48d08c01
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-11-26 00:46:13 -0600

    ad;lfkj

--- home.php
+++ home.php
@@ -3,6 +3,10 @@
     <head>
        <?php 
             session_start();
+            if(!$_SESSION['loggedin']){
+                header("Location: login.php");
+                exit;
+            }
             print '<a href="logout.php">Logout</a>';
             print "<h1> Welcome " . $_SESSION['loggedin'] . " </h1>";    
         ?>
