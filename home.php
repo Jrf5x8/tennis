2b15452f7a536cb17a655decd745d6dfb8895e23
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-11-26 00:40:39 -0600

    adj;

--- home.php
+++ home.php
@@ -11,7 +11,7 @@
         <script>
             $(function(){
                 $.get("homeData.php", function(data){
-                    $('#test').html(data.name);
+                    $('#test').html(data);
                 });
             });
                     
