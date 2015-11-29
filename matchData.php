698da9518e392dfb4d1c516d0801337dfd3723a2
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-11-28 21:04:58 -0600

    a;dfj

--- matchData.php
+++ matchData.php
@@ -12,10 +12,10 @@
     session_start();
     //get username
     $username = $_SESSION['loggedin'];
-    //$tourney = $_GET['tourney'];
+    $tourney = $_GET['tourney'];
     
 
-    $query = "SELECT fname FROM person JOIN matches on person.id = matches.player2 WHERE matches.player1 = (SELECT id FROM person WHERE fname = '$username')";
+    $query = "SELECT fname FROM person JOIN matches on person.id = matches.player2 WHERE matches.player1 = (SELECT id FROM person WHERE fname = '$username') AND matches.tournID = (SELECT id FROM tournaments WHERE name = '$tourney')";
     $opponents = $mysqli->query($query);
     header('Content-Type: application/json');
 
