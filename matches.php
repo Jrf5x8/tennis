d371c397c8b67df249585efb0fe8acd3bfee72a9
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-11-29 23:48:08 -0600

    a;dfj

--- matches.php
+++ matches.php
@@ -19,7 +19,6 @@
         <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
         <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
         <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
-        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
 
         <script>
            $(function(){
