51029e9dc5debf619392cc4210b6cb0d214c915e
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-11-29 19:40:42 -0600

    a;dfj

---
+++ matchData.php
@@ -0,0 +1,33 @@
+<?php
+    
+    require_once 'db.conf';
+
+
+    $mysqli = new mysqli($dbhost, $dbuser, $dbpass, $dbname);
+
+    if($mysqli->connect_error){
+        $error = 'Error: ' . $mysqli->connect_errno . ' ' . $mysqli->connect_error;
+        require "login_form.php";
+        exit;
+    }
+    session_start();
+    //get username
+    $username = $_SESSION['loggedin'];
+    $tourney = $_GET['tourney'];
+    
+    $query = "SELECT fname FROM person JOIN matches on person.id = matches.player2 WHERE matches.player1 = (SELECT id FROM person WHERE fname = '$username') AND matches.tournID = (SELECT id FROM tournaments WHERE name = '$tourney')";
+
+//    $query = "SELECT fname FROM person JOIN matches on person.id = matches.player2 WHERE matches.player1 = (SELECT id FROM person WHERE fname = '$username')";
+
+    $opponents = $mysqli->query($query);
+    header('Content-Type: application/json');
+
+    $result = json_encode(mysqli_fetch_all($opponents,MYSQLI_ASSOC));
+
+    
+//    $result->close();
+//    $mysqli->close();
+
+    print $result;
+
+?>
---
+++ matches.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css.css">
+
+       <?php 
+            session_start();
+            if(!$_SESSION['loggedin']){
+                header("Location: login.php");
+                exit;
+            }
+            print '<a href="logout.php">Logout</a>';
+               
+        ?>
+        
+        <title>Home Page</title>
+        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script>
+           $(function(){
+                $.get("matchData.php", {"tourney": "<?php print $_GET['tourney']; ?>"}, function(data){
+                    console.log(data);
+                    for(var d in data){
+                        var li = $("<li>");
+                        li.html(data[d]["fname"])
+                        .click(function(){
+                            $.get("scores.php", {"opponent":$(this).text()}, function(data){
+                                $("#test").html(data);
+                            });
+                            
+                        });
+                        $("#test ul").append(li);
+                    }
+                });  
+            });
+                    
+        </script>
+    </head>
+    <body>
+        <div id="wrapper">
+            <div id="header"></div>
+            <div id="test">
+                <ul id="tournamentList">
+                    
+                </ul>
+            </div>
+        </div>
+    </body>
+    
+</html>
