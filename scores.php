ab5d7ebfb5f3079ad09fc584b269229dc405d5d6
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-11-29 20:52:30 -0600

    a;dfj

--- scores.php
+++ scores.php
@@ -12,22 +12,20 @@
 
     print '<form action="reportScore.php" method="POST">
                 <p>Score:</p>
-                <div id="inline">
                     <table>
                     <tr>
                         <td><label for="set1games">1st Set: </label></td>
                         <td><input type="number" name="set1games" min="0" max="7" maxlength="1" size="3"> - </td>
                         <td><input type="number" name="set1games2" min="0" max="7" maxlength="1" size="3"></td>
                     </tr>
-                    </div>
-                <div id="inline">
                     <tr>
                         <td><label for="set2games">2nd Set: </label></td>
                         <td><input type="number" name="set2games" min="0" max="7" maxlength="1" size="3"> - </td>
                         <td><input type="number" name="set2games" min="0" max="7" maxlength="1" size="3"></td>
                     </tr>
-                </div>
-                <input type="submit" value="submit">
+                    <tr>
+                        <input type="submit" value="submit">
+                    </tr>
             </form>'
 
 ?>
