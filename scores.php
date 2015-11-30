7de26f326bb0764a20de688b78ad01782909ec75
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-11-29 22:04:59 -0600

    a;dfj

--- scores.php
+++ scores.php
@@ -1,9 +1,9 @@
 <?php
 
-    $opponent = $_GET['opponent']
+    $opponent = $_GET['opponent'];
 
     print '<form id="scores" method="post" action="reportScores.php">
-                <p>Score vs <span id="opponent">Dan</span>:</p>
+                <p>Score vs <span id="opponent">' . $opponent . '</span>:</p>
                     <table>
                     <tr>
                         <td><label for="set1games">1st Set: </label></td>
