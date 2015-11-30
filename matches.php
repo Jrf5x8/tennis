b233f788d2b37cdf63cd2044a42a581adaad9100
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-11-30 15:50:32 -0600

    a;dfj

--- matches.php
+++ matches.php
@@ -53,7 +53,7 @@
                 });
                
                $("#accordion").click(function(){
-                    $("#opponent").attr("value", $(this).text());
+                    $("#opponent").attr("value", $(this).children("h3").text());
                });
 
            });
