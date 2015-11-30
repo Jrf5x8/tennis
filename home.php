15d60c6dc1acd1a6c24b53db8f192b3e0a4e9aa5
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-11-30 00:15:54 -0600

    a;dfj

--- home.php
+++ home.php
@@ -14,9 +14,9 @@
         ?>
         
         <title>Home Page</title>
-        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
-        <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+        <script src="//code.jquery.com/jquery-1.11.4.min.js"></script>
+        <link rel="stylesheet" href="jquery-ui-1.11.4.custom/jquery-ui.min.css">
+        <script src="jquery-ui-1.11.4.custom/jquery-ui.min.js"></script>
         <script>
             $(function(){
                 $("#accordion").accordion();
