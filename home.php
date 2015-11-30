c2d762da45e87869bd4273ed8d1a0782c0d2b505
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-11-29 23:46:55 -0600

    a;dfj

--- home.php
+++ home.php
@@ -15,8 +15,9 @@
         
         <title>Home Page</title>
         <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
-        <script src="jquery-ui-1.11.4.custom/jquery-ui.min.js"></script>
-        <link rel="stylesheet" type="text/css" href="jquery-ui-1.11.4.custom/jquery-ui.min.css">
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+        <script src="//code.jquery.com/ui/1.11.4/jquery-ui.js"></script>
+        <script src="//code.jquery.com/jquery-1.10.2.js"></script>
         <script>
             $(function(){
                 $("#header").html("<?php print "<h1> Welcome " . $_SESSION['loggedin'] . " </h1>"; ?>");
