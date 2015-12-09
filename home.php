1fef46b6fccc5866923783e7ac8234d3db8e3a18
Jrf5x8 <jrf5x8@mail.missouri.edu>
2015-12-09 13:09:40 -0600

    upload

--- home.php
+++ home.php
@@ -55,11 +55,11 @@
     </head>
     <body>
         <div id="wrapper">
-            <div><?php print '<a href="logout.php" id="logout">Logout</a>';
+            <div id="nav"><?php print '<a href="logout.php" id="logout">Logout</a>';
                 print '<span class="clicked"><a href="home.php">Home</a></span>';
-                print '<a href="tournamentMedia.php">Media</a>'; ?></div>
-            <div id="loading"></div>
-            <div id="header"></div>  
+                print '<a href="tournamentMedia.php">Media</a>'; ?></div>       
+            <div id="header"></div> 
+            <div id="loading"></div> 
             <div id="test"><ul></ul></div>        
         </div>
     </body>
